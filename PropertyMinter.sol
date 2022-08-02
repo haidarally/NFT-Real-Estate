842d5811d7b9ec31c139bc9e42535a9a9315d1fe
Haidar Deenmahomed <haidar.deenmahomed@rapp.com>
2022-08-02 18:53:25 +0400

    updated contract

--- PropertyMinter.sol
+++ PropertyMinter.sol
@@ -4,6 +4,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 
 
@@ -33,6 +34,10 @@ contract PropertyMinter is ERC721,ERC721URIStorage, Ownable, AccessControl {
         Status status;
         uint256 tokenId;
     }
+
+    mapping (string=> PropertyStruct[]) public UserProperties;
+    string[] public userList;
+    uint256 public arrayLength;
 
 
     constructor(address[] memory notaries) ERC721 (tokenName,tokenTicker) public {
@@ -66,14 +71,14 @@ contract PropertyMinter is ERC721,ERC721URIStorage, Ownable, AccessControl {
 
     function mint(string memory _tokenURI, address _to,
     uint256 _date, string memory _owner, string memory _previousOwner,
-    string memory _ownerContact, string memory _propertyAddress, string memory _PIN, Status status   
+    string memory _ownerContact, string memory _propertyAddress, string memory _PIN 
     ) public onlyRole(NOTARY_ROLE) returns (uint256){
         uint256 realEstateAsset = propertyCounter;
         _safeMint(_to, realEstateAsset);
         _setTokenURI(realEstateAsset,_tokenURI);
 
         //Add Data
-        AddData("(string(_to)",_date, _owner,  _previousOwner,_ownerContact,  _propertyAddress, _PIN, Status.active,realEstateAsset);
+        AddData(_to,_date, _owner,  _previousOwner,_ownerContact,  _propertyAddress, _PIN, Status.active,realEstateAsset);
 
         propertyCounter+=1;
 
@@ -89,17 +94,22 @@ contract PropertyMinter is ERC721,ERC721URIStorage, Ownable, AccessControl {
         _burn(_tokenId);
 
         //change status of the property
-
+        UpdateStatusData(msg.sender,_tokenId);
         //update data
 
     }
 
-    function _transfer(address from, address to, uint256 tokenId) internal virtual override {
+    function _transfer(address from, address to, uint256 tokenId, 
+    uint256 _date, string memory _owner, string memory _previousOwner,
+    string memory _ownerContact, string memory _propertyAddress, string memory _PIN) internal virtual  {
         super._transfer(from,to,tokenId);
 
         //addData
+        
+        AddData(to,_date, _owner,  _previousOwner,_ownerContact,  _propertyAddress, _PIN, Status.active,tokenId);
 
         //updateData
+        UpdateStatusData(from,tokenId);
     }
 
     //helper
@@ -113,13 +123,11 @@ contract PropertyMinter is ERC721,ERC721URIStorage, Ownable, AccessControl {
     }
 
 
-    mapping (string=> PropertyStruct[]) public UserProperties;
-    string[] public userList;
-    uint256 public arrayLength;
 
-    function AddData(string memory _address, uint256 _date, string memory _owner, string memory _previousOwner,
+
+    function AddData(address _addressX, uint256 _date, string memory _owner, string memory _previousOwner,
     string memory _ownerContact, string memory _propertyAddress, string memory _PIN, Status status, uint256 tokenId) internal {
-
+        string memory _address = Strings.toHexString(uint256(uint160(_addressX)), 20);
         int arrayIndex = indexOf(userList,_address);
         if(arrayIndex<0){
             userList.push(_address);
@@ -142,12 +150,14 @@ contract PropertyMinter is ERC721,ERC721URIStorage, Ownable, AccessControl {
         
     }
 
-    function GetUserProperties (string memory _address) public view returns (PropertyStruct[] memory propertyStructsUser){
+    function GetUserProperties (address _addressX) public view returns (PropertyStruct[] memory propertyStructsUser){
+        string memory _address = Strings.toHexString(uint256(uint160(_addressX)), 20);
         return UserProperties[_address];
     }
 
-    function UpdateStatusData(string memory _address,uint256 _tokenId) internal {
+    function UpdateStatusData(address _addressX,uint256 _tokenId) internal {
         //ProperyStruct[] memory userProperties = UserProperties[_address];
+        string memory _address = Strings.toHexString(uint256(uint160(_addressX)), 20);
         PropertyStruct[] memory propertyStructsUser_ = UserProperties[_address];
            
         for (uint i = 0; i < propertyStructsUser_.length; i++) {
